52a9129345db3ef44800afc1940428727d1b7fcd
fermarja <fermarja@jagsc0001p142.jcyl.red>
2020-11-03 13:18:07 +0100

    Estructura inicial de paquetes

---
+++ src/main/java/es/jcyl/cag/cursotesting/fizzbuzz/Main.java
@@ -0,0 +1,10 @@
+package es.jcyl.cag.cursotesting.fizzbuzz;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ src/test/java/es/jcyl/cag/cursotesting/fizzbuzz/FizzBuzzTest.java
@@ -0,0 +1,5 @@
+package es.jcyl.cag.cursotesting.fizzbuzz;
+
+public class FizzBuzzTest {
+
+}
