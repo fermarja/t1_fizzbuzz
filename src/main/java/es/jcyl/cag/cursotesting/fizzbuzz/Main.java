cbb575d6c0bb79c2fdab503144cbcb1e7cf4ea57
fermarja <fermarja@jagsc0001p142.jcyl.red>
2020-11-03 13:54:44 +0100

    Implementacion de la clase Main

--- src/main/java/es/jcyl/cag/cursotesting/fizzbuzz/Main.java
+++ src/main/java/es/jcyl/cag/cursotesting/fizzbuzz/Main.java
@@ -3,8 +3,18 @@ package es.jcyl.cag.cursotesting.fizzbuzz;
 public class Main {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		FizzBuzz conversor = createFizzBuzz();
+		for (int i = 1; i <= 100; i++) {
+			System.out.println(conversor.convertToString(i));
+		}
 	}
 
+	private static FizzBuzz createFizzBuzz() {
+		FizzBuzz conversor = new FizzBuzz();
+		conversor.addConverter(new FizzBuzzConverter());
+		conversor.addConverter(new FizzConverter());
+		conversor.addConverter(new BuzzConverter());
+		conversor.addConverter(new DefaultConverter());
+		return conversor;
+	}
 }
