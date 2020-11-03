86de721da48dd89a99ce0ea48bb3b339419a3b88
fermarja <fermarja@jagsc0001p142.jcyl.red>
2020-11-03 20:20:09 +0100

    Refactor de los tests para recibir lista de parametros con JUnitParams

--- src/test/java/es/jcyl/cag/cursotesting/fizzbuzz/FizzBuzzTest.java
+++ src/test/java/es/jcyl/cag/cursotesting/fizzbuzz/FizzBuzzTest.java
@@ -2,7 +2,12 @@ package es.jcyl.cag.cursotesting.fizzbuzz;
 
 import org.junit.Assert;
 import org.junit.Test;
+import org.junit.runner.RunWith;
 
+import junitparams.JUnitParamsRunner;
+import junitparams.Parameters;
+
+@RunWith(JUnitParamsRunner.class)
 public class FizzBuzzTest {
 
 	public FizzBuzzTest () {
@@ -11,48 +16,59 @@ public class FizzBuzzTest {
 	
 	
 	@Test(expected = IllegalArgumentException.class)
-	public void testCero() {
+	public void testCeroNoPermitido() {
 		convert(0);
 	}
 	
-	@Test
-	public void testUno() {
-		Assert.assertEquals("1", convert(1));
+	@Test(expected = IllegalArgumentException.class)
+	public void testNegativosNoPermitidos() {
+		convert(-1);
 	}
+	
+	
+	  
 	@Test
-	public void testDos() {
-		Assert.assertEquals("2", convert(2));
+	@Parameters({"1", "2", "8"})
+	public void testNumerosNoMultiposDe3_5_7(int number) {
+		Assert.assertEquals(Integer.toString(number), convert(number));
+	}
+
+	
+	@Test
+	@Parameters({"3", "6", "9"})
+	public void testMultiposDe3Fizz(int number) {
+		Assert.assertEquals("Fizz", convert(number));
 	}
 	
 	@Test
-	public void testTres() {
-		Assert.assertEquals("Fizz", convert(3));
-	}
-	
-	@Test
-	public void tresCinco() {
-		Assert.assertEquals("Buzz", convert(5));
+	@Parameters({"5", "10", "20"})
+	public void testMultiplosDe5Buzz(int number) {
+		Assert.assertEquals("Buzz", convert(number));
 	}
 	
 	@Test 
-	public void testQuince() {
-		Assert.assertEquals("FizzBuzz?", convert(15));
+	@Parameters({"15", "30", "45"})
+	public void testMultiplosDe3y5FizzBuz(int number) {
+		Assert.assertEquals("FizzBuzz?", convert(number));
 	}
 	
 	
 	@Test
-	public void testModificacionFizzSiTiene3() {
-		Assert.assertEquals("Fizz", convert(13));
+	@Parameters({"13", "23", "31"})
+	public void testContiene3Fizz(int number) {
+		Assert.assertEquals("Fizz", convert(number));
 	}
 	
 	@Test
-	public void testModifcacionBuzzSiTiene5() {
-		Assert.assertEquals("Buzz", convert(52));
+	@Parameters({"52", "58"})
+	public void testContinene5Buzz(int number) {
+		Assert.assertEquals("Buzz", convert(number));
 	}
 	
 	@Test
-	public void testModificacionWhizzMultiplos7() {
-		Assert.assertEquals("Whizz", convert(7));
+	@Parameters({"7", "14"})
+	public void testWitzzParaMultiplosDe7(int number) {
+		Assert.assertEquals("Whizz", convert(number));
 	}
 	
 	
